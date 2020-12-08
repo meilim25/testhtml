753a4d185524fe344d96b62c5af61455b2c87e44
meilim25 <75291535+meilim25@users.noreply.github.com>
2020-12-08 12:14:38 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -1,4 +1,25 @@
-<?php
+<html>
+<head>
+<title>List of KL Restaurant</title>
+</head>
+<body>
+<center>
+<h1>List of KL Restaurant</h1>
+<!--create table structure using HTML first-->
+<table border="1">
+<tr>
+<th>Restaurant ID</th>
+<th>Restaurant Name</th>
+<th>Address</th>
+<th>Phone</th>
+</tr>
+<tr>
+<th>0</th>
+<th>Subway Restaurant</th>
+<th>Menara Standard Chartered, TPM</th>
+<th>03-22441234</th>
+</tr>
+  <?php
 $serverName = "simplewebtp000000.database.windows.net";
 $connectionOptions = array(
 "Database" => "simpleweb",
@@ -27,3 +48,7 @@ echo "</tr>";
 }
 sqlsrv_free_stmt($getResults);
 ?>
+</table>
+</center>
+</body>
+</html>
